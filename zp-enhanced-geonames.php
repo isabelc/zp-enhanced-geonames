1d91ab80c108a7bb82020f16fcc3bb1282220d40
Isabel C <isabel@isabelcastillo.com>
2017-08-30 22:25:49 -0400

    zp_timezone_ajaxurl filter is only for back compat

--- zp-enhanced-geonames.php
+++ zp-enhanced-geonames.php
@@ -1,15 +1,15 @@
 <?php
-/**
+/*
 Plugin Name: ZodiacPress Enhanced GeoNames
 Plugin URI:	https://cosmicplugins.com/downloads/zodiacpress-enhanced-geonames/
 Description: Improves the experience when filling out the Birth City field on the ZP form.
-Version: 1.0
+Version: 1.5
 Author:	Isabel Castillo
-Author URI:	http://isabelcastillo.com
+Author URI:	https://isabelcastillo.com
 License: GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
-Copyright 2106 Isabel Castillo
+Copyright 2016-2017 Isabel Castillo
 
 ZodiacPress Enhanced GeoNames is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -31,9 +31,6 @@ if ( class_exists( 'ZP_License' ) && is_admin() ) {
 function zppg_autocomplete_ajaxurl( $url ) {
 	return '//api.geonames.org/searchJSON';
 }
-function zppg_timezone_ajaxurl( $url ) {
-	return '//api.geonames.org/timezoneJSON';
-}
 function zppg_ajax_datatype( $type ) {
 	return 'jsonp';
 }
@@ -41,7 +38,16 @@ function zppg_ajax_type( $type ) {
 	return 'GET';
 }
 add_filter( 'zp_autocomplete_ajaxurl', 'zppg_autocomplete_ajaxurl' );
-add_filter( 'zp_timezone_ajaxurl', 'zppg_timezone_ajaxurl' );
 add_filter( 'zp_ajax_geonames_action', '__return_false'  );
 add_filter( 'zp_ajax_datatype', 'zppg_ajax_datatype' );
 add_filter( 'zp_ajax_type', 'zppg_ajax_type' );
+/**
+ * For ZP versions less than 1.5.4
+ * @todo remove this in the future.
+ */
+if ( version_compare( ZODIACPRESS_VERSION, '1.5.4', '<' ) ) {
+	function zppg_timezone_ajaxurl( $url ) {
+		return '//api.geonames.org/timezoneJSON';
+	}
+	add_filter( 'zp_timezone_ajaxurl', 'zppg_timezone_ajaxurl' );
+}
